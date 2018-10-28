bd0d23b357fbad1a28c2a77fe5f1a21394ef9e76
ThanhTung1235 <tungltd00602@fpt.edu.vn>
2018-10-28 23:46:39 +0700

    No Hope

--- Search-my-youtube/YoutubeJS.js
+++ Search-my-youtube/YoutubeJS.js
@@ -1,36 +1,60 @@
+var modal = document.getElementById("myModal");
+var img = document.getElementById("myImg");
+var modalImg = document.getElementById("frameSrc");
 
-
-
-// var YOUTUBE_API = "https://content.googleapis.com/youtube/v3/search?q="+ keyword + "&type=video&maxResults=9&part=snippet&key=AIzaSyAwUjk3CwtXCiB_W6Xi0colfOKPgm90hHc";
-var xhttp = new XMLHttpRequest();
-xhttp.onreadystatechange = function () {
-    if (this.readyState == 4 && this.status == 200) {
-        var jsonObj = JSON.parse(xhttp.responseText);
-        console.log(xhttp.responseText);
-        var content = '';
-        // for (var i = 0; i < jsonObj.items.length; i++) {
-        //     var videoItem = '<div class="tube-item">';
-        //     videoItem += '<iframe width="660" height="355" src="https://www.youtube.com/embed/' + jsonObj.items[i].id.videoId + '" frameborder="0"  allow="
-        //
-        // ; encrypted-media" allowfullscreen></iframe>';
-        //     videoItem += '<h3>' + jsonObj.items[i].snippet.title+ '</h3>';
-        //     videoItem += '</div>';
-        //     content += videoItem;
-        // }
-        document.getElementById("myTubes").innerHTML = xhttp.responseText;
-    }
-
-
-};
-
-function btn_search() {
+function Search_youtube() {
     var txt_search = document.getElementById("search");
     var keyword = txt_search.value;
-    var YOUTUBE_API = "https://content.googleapis.com/youtube/v3/search?q="+ keyword + "&type=video&maxResults=9&part=snippet&key=AIzaSyAwUjk3CwtXCiB_W6Xi0colfOKPgm90hHc";
+    var YOUTUBE_API = "https://content.googleapis.com/youtube/v3/search?q=" + keyword + "&type=video&maxResults=9&part=snippet&key=AIzaSyAwUjk3CwtXCiB_W6Xi0colfOKPgm90hHc";
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function GetInfo() {
+        if (this.readyState == 4 && this.status == 200) {
+            var jsonObj = JSON.parse(xhttp.responseText);
+            console.log(xhttp.responseText);
+            var content = '';
+            for (var i = 0; i < jsonObj.items.length; i++) {
+                var videoItem = '<div class="tube-item">';
+                // videoItem += '<iframe width="660" height="355" src="https://www.youtube.com/embed/' + jsonObj.items[i].id.videoId + '" frameborder="0"  allow=" encrypted-media" allowfullscreen></iframe>';
+                videoItem += '<img id="myImg" style="width: 100%;max-width: 300px" onclick="DoAction( ' + jsonObj.items[i].id.videoId +  ')" src="' + jsonObj.items[i].snippet.thumbnails.high.url + '">';
+                videoItem += '<h3>' + jsonObj.items[i].snippet.title + '</h3>';
+                videoItem += '</div>';
+                content += videoItem;
+            }
+            document.getElementById("myTubes").innerHTML = content;
+        }
 
-    xhttp.open("GET",YOUTUBE_API,true);
+
+    };
+    xhttp.open("GET", YOUTUBE_API, true);
     xhttp.send();
 }
+
+function DoAction(videoId) {
+    modal.style.display = "block";
+    modalImg.src = "https://www.youtube.com/embed/"+videoId;
+}
+
+function btn_search() {
+    Search_youtube();
+}
+
+
+// var modal = document.getElementById("myModal");
+// var img = document.getElementById("myImg");
+// var modalImg = document.getElementById("frameSrc");
+// img.onclick = function () {
+//     modal.style.display = "block";
+//     modalImg.src = this.src;
+// }
+var span = document.getElementsByClassName("close")[0];
+span.onclick = function () {
+    modal.style.display = "none";
+}
+
+
+
+
+
 
 
 
