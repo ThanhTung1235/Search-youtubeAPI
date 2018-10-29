ee5de30831b06d2dfbb49aac7fb6602c135fa366
ThanhTung1235 <tungltd00602@fpt.edu.vn>
2018-10-29 19:11:41 +0700

    No hope 2nd

--- Search-my-youtube/YoutubeJS.js
+++ Search-my-youtube/YoutubeJS.js
@@ -1,6 +1,11 @@
 var modal = document.getElementById("myModal");
-var img = document.getElementById("myImg");
 var modalImg = document.getElementById("frameSrc");
+Search_youtube();
+
+
+function btn_search() {
+    Search_youtube();
+}
 
 function Search_youtube() {
     var txt_search = document.getElementById("search");
@@ -10,12 +15,12 @@ function Search_youtube() {
     xhttp.onreadystatechange = function GetInfo() {
         if (this.readyState == 4 && this.status == 200) {
             var jsonObj = JSON.parse(xhttp.responseText);
-            console.log(xhttp.responseText);
+            // console.log(xhttp.responseText);
             var content = '';
             for (var i = 0; i < jsonObj.items.length; i++) {
                 var videoItem = '<div class="tube-item">';
                 // videoItem += '<iframe width="660" height="355" src="https://www.youtube.com/embed/' + jsonObj.items[i].id.videoId + '" frameborder="0"  allow=" encrypted-media" allowfullscreen></iframe>';
-                videoItem += '<img id="myImg" style="width: 100%;max-width: 300px" onclick="DoAction( ' + jsonObj.items[i].id.videoId +  ')" src="' + jsonObj.items[i].snippet.thumbnails.high.url + '">';
+                videoItem += '<img id="myImg" style="width: 100%;max-width: 300px" onclick="DoAction( /' + jsonObj.items[i].id.videoId + '/)" alt="" src="' + jsonObj.items[i].snippet.thumbnails.high.url + '">';
                 videoItem += '<h3>' + jsonObj.items[i].snippet.title + '</h3>';
                 videoItem += '</div>';
                 content += videoItem;
@@ -28,27 +33,15 @@ function Search_youtube() {
     xhttp.open("GET", YOUTUBE_API, true);
     xhttp.send();
 }
-
 function DoAction(videoId) {
     modal.style.display = "block";
-    modalImg.src = "https://www.youtube.com/embed/"+videoId;
+    modalImg.src = 'https://www.youtube.com/embed/'+videoId;
+    alert("https://www.youtube.com/embed/"+videoId);
 }
-
-function btn_search() {
-    Search_youtube();
-}
-
-
-// var modal = document.getElementById("myModal");
-// var img = document.getElementById("myImg");
-// var modalImg = document.getElementById("frameSrc");
-// img.onclick = function () {
-//     modal.style.display = "block";
-//     modalImg.src = this.src;
-// }
 var span = document.getElementsByClassName("close")[0];
 span.onclick = function () {
     modal.style.display = "none";
+    modalImg.src = "";
 }
 
 
